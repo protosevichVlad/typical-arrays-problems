a63d2eb8d00f5eb507dd30c40f1f5ae1c2d6216a
Protosevich <protosevich2001@gmail.com>
2020-02-19 22:37:24 +0300

    feat: full solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array == undefined || array.length == 0) return 0;
+  let min = array[0];
+  for(let i = 1; i < array.length; i++){
+    if(min > array[i]){
+      min = array[i]
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array == undefined || array.length == 0) return 0;
+  let max = array[0];
+  for(let i = 1; i < array.length; i++){
+    if(max < array[i]){
+      max = array[i]
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array == undefined || array.length == 0) return 0;
+  return array.reduce((a, b) => a + b) / array.length;
 }
